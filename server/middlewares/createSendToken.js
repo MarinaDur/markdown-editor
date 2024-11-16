94ebe3c8a1b849a4b5f3226485c97c3b44e230b1
Marina Dur <marinadur21@gmail.com>
2024-11-16 22:33:03 +0200

    fix cookie config

--- server/middlewares/createSendToken.js
+++ server/middlewares/createSendToken.js
@@ -13,7 +13,7 @@ export const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: false,
-    secure: false,
+    secure: true,
     sameSite: "none",
     path: "/",
   };
