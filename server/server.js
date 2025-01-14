55b6a42e362b3fba4f1ed4f9bf693fd758c86856
Marina Dur <marinadur21@gmail.com>
2025-01-14 23:08:14 +0200

    remove SSL option from mongoose connection

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const DB = process.env.DATABASE;
 // );
 
 mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, ssl: true })
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("SUCCESSFULLY CONNECTED"))
   .catch((err) => console.log(err));
 
