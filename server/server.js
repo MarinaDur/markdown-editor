1bf8add0174ad8ce6fdaf50b2dc516c5997eac16
Marina Dur <marinadur21@gmail.com>
2025-01-14 23:06:15 +0200

    enable SSL for mongoose connection

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const DB = process.env.DATABASE;
 // );
 
 mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true, ssl: true })
   .then(() => console.log("SUCCESSFULLY CONNECTED"))
   .catch((err) => console.log(err));
 
