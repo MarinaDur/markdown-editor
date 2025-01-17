ec1d2ba938ed4822be287a1e184fef3d1d049be6
Marina Dur <marinadur21@gmail.com>
2025-01-17 18:04:19 +0200

    fix: update API endpoint paths to remove versioning from URLs

--- client/src/utils/apiCalls.ts
+++ client/src/utils/apiCalls.ts
@@ -2,18 +2,18 @@ import axios from "axios";
 import { UpdateDocumentVariables } from "../interfaces/documets";
 import { Email, ResetToken, UserLogin, UserSignUp } from "../interfaces/users";
 
-const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
+const BASE_URL = `${import.meta.env.VITE_REACT_APP_API_URL}/api/v1`;
 // console.log("BASE_URL", BASE_URL);
 
 export async function fetchDocuments() {
-  const response = await axios.get(`${BASE_URL}/v1/documents/getUserDocs`, {
+  const response = await axios.get(`${BASE_URL}/documents/getUserDocs`, {
     withCredentials: true,
   });
   return response?.data?.data?.data; // Adjust based on your API's response structure
 }
 
 export async function fetchOneDocuments(docId: string) {
-  const response = await axios.get(`${BASE_URL}/v1/documents/${docId}`, {
+  const response = await axios.get(`${BASE_URL}/documents/${docId}`, {
     withCredentials: true,
   });
   return response; // Adjust based on your API's response structure
@@ -25,7 +25,7 @@ export async function updateDocument({
   content,
 }: UpdateDocumentVariables): Promise<any> {
   const response = await axios.patch(
-    `${BASE_URL}/v1/documents/${id}`,
+    `${BASE_URL}/documents/${id}`,
     { name, content },
     {
       headers: { "Content-Type": "application/json" },
@@ -46,7 +46,7 @@ export async function createDocument({
   content,
 }: UpdateDocumentVariables): Promise<any> {
   const response = await axios.post(
-    `${BASE_URL}/v1/documents/postDoc`,
+    `${BASE_URL}/documents/postDoc`,
     { name, content },
     {
       headers: { "Content-Type": "application/json" },
@@ -66,7 +66,7 @@ export async function deleteDocument({
   id,
 }: UpdateDocumentVariables): Promise<any> {
   const response = await axios.delete(
-    `${BASE_URL}/v1/documents/${id}`,
+    `${BASE_URL}/documents/${id}`,
 
     {
       headers: { "Content-Type": "application/json" },
@@ -84,7 +84,7 @@ export async function deleteDocument({
 
 export async function login({ email, password }: UserLogin): Promise<any> {
   const response = await axios.post(
-    `${BASE_URL}/v1/users/login`,
+    `${BASE_URL}/users/login`,
     {
       email,
       password,
@@ -99,7 +99,7 @@ export async function login({ email, password }: UserLogin): Promise<any> {
 }
 
 export async function logout() {
-  const response = await axios.get(`${BASE_URL}/v1/users/logout`, {
+  const response = await axios.get(`${BASE_URL}/users/logout`, {
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
   });
@@ -113,7 +113,7 @@ export async function logout() {
 }
 
 export async function authMe() {
-  const response = await axios.get(`${BASE_URL}/v1/users/getMe`, {
+  const response = await axios.get(`${BASE_URL}/users/getMe`, {
     headers: { "Content-Type": "application/json" },
     withCredentials: true,
   });
@@ -133,7 +133,7 @@ export async function signup({
   passwordConfirm,
 }: UserSignUp): Promise<any> {
   const response = await axios.post(
-    `${BASE_URL}/v1/users/signup`,
+    `${BASE_URL}/users/signup`,
     {
       name: userName,
       email,
@@ -151,7 +151,7 @@ export async function signup({
 
 export async function forgotPassword({ email }: Email): Promise<any> {
   const response = await axios.post(
-    `${BASE_URL}/v1/users/forgotPassword`,
+    `${BASE_URL}/users/forgotPassword`,
     {
       email,
     },
@@ -170,7 +170,7 @@ export async function resetPassword({
   passwordConfirm,
 }: ResetToken): Promise<any> {
   const response = await axios.patch(
-    `${BASE_URL}/v1/users/resetPassword/${token}`,
+    `${BASE_URL}/users/resetPassword/${token}`,
     {
       password,
       passwordConfirm,
