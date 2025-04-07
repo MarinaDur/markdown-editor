19c40b42f491c531766053acf6734dad9f868725
Marina Dur <marinadur21@gmail.com>
2025-04-07 20:59:05 +0300

    fix: add 'test' label to server startup log message

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ mongoose
 const port = process.env.PORT || 3000
 
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+  console.log(`App running on port ${port}`, 'test')
 })
 
 process.on('unhandledRejection', (err) => {
