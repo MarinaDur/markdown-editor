eb16dddec6c435e1059861bf85ee9bfe6ee922ac
Marina Dur <marinadur21@gmail.com>
2025-04-07 21:41:42 +0300

    fix: update global error handler to use underscore for 'next' parameter

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -121,7 +121,7 @@ const cloneError = (err) => {
 }
 
 // Main Error Handler
-const globalErrorHandler = (err, req, res, next) => {
+const globalErrorHandler = (err, req, res, _next) => {
   console.log('Caught Error:', err.statusCode)
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
